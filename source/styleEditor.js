ca3dff006d5cc1a77fb9a0d52daa28e9c58b4a79
TMad <madsen.tyler@gmail.com>
2018-03-10 13:47:45 -0700

    add jquery for form style editor

--- source/styleEditor.js
+++ source/styleEditor.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-  // your code goes here.
+  $('#style_editor').submit((event) => {
+    event.preventDefault();
+    let selector = $("input[name='selector']").val();
+    let property = $("input[name='property']").val();
+    let value = $("input[name='value']").val();
+    $(`${selector}`).css(`${property}`,`${value}`)
+  })
 });
